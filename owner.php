9ffe89e4fcf94c8101da9500bf7ffbea13e44ee7
nmuur <112870463+nmuur@users.noreply.github.com>
2022-10-01 10:43:53 +0200

    Create owner.php

---
+++ owner.php
@@ -0,0 +1,26 @@
+<?php
+    class Owner{
+        private ?string $middlename = null;
+
+        public function __constuct(
+            private string $ownername,
+            private string $surname,
+            private string $number,
+            private string $email,
+        ){}
+        public function getOwnername() : string {
+            return $this->ownername;
+        }
+        public function getSurname() : string {
+            return $this->surname;
+        }
+        public function getNumber() : string {
+            return $this->number;
+        }
+        public function getEmail() : string {
+            return $this->email;
+        }
+        public function setMiddlename(string $middlename) : void {
+            $this->middlename=$middlename;
+        }
+}    
